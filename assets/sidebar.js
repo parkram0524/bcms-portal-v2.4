7a1eff17bcdbac7537d67775b4a18e0057a9f330
Park <parkram0@gmail.com>
2026-05-29 09:03:29 +0900

    작업 완료: 3개 파일 변경 (assets/sidebar.js, governance/org-registry.html, governance/org.html)

--- assets/sidebar.js
+++ assets/sidebar.js
@@ -104,8 +104,8 @@
       <a href="${H("/governance/index.html")}" class="sidebar-section-title">🧭 1. Governance (거버넌스)</a>
       <div class="sub-menu">
         <a href="${H("/governance/policy.html")}">정책/목표/범위</a>
-        <a href="${H("/governance/org-registry.html")}">조직 관리</a>
         <a href="${H("/governance/org.html")}">조직도 (평상시/비상대응)</a>
+        <a href="${H("/governance/org-registry.html")}">조직 관리</a>
         <a href="${H("/governance/docs.html")}">문서체계</a>
         <a href="${H("/governance/requirements.html")}">법규·요구사항</a>
         <a href="${H("/governance/bcms-map.html")}">BCMS 체계 맵</a>
